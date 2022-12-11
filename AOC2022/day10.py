2c7b2b299c631fcbcdd49c25d908501d230bb8f6
ntdkhang <ntdkhang143@gmail.com>
2022-12-11 16:20:17 -0600

    day 10 part 2

--- AOC2022/day10.py
+++ AOC2022/day10.py
@@ -4,18 +4,20 @@ valueAt = [0] * 1000
 
 currentCycle = 1
 currentX = 1
+valueAt[1] = 1
 
 for line in file:
     # print(currentCycle, currentX)
-    valueAt[currentCycle] = currentX
     if line[:4] == "noop":
         currentCycle += 1
     else:
         # print(line.split()[1])
+        valueAt[currentCycle] = currentX
         valueAt[currentCycle + 1] = currentX
-        valueAt[currentCycle + 2] = currentX
         currentX += int(line.split()[1])
         currentCycle += 2
+
+    valueAt[currentCycle] = currentX
 
 signalStrengths = 0
 for i in range(20, 221, 40):
@@ -28,9 +30,20 @@ print(signalStrengths)
 
 # cycle starts at 1
 # pixel index starts at 0
-# for row in range(6):
-#     for col in range(1, 41):
-#         cycle = row * 40 + col
-#         if valueAt[cycle]  
+pixels = []
+for row in range(6):
+    pixelRow = ""
+    for col in range(1, 41):
+        cycle = row * 40 + col
+        sprite = range(valueAt[cycle], valueAt[cycle] + 3)
+        # is the first cycle affected?
+        if col in sprite:
+            pixelRow += "#"
+        else:
+            pixelRow += "."
+    pixels.append(pixelRow)
 
+print(valueAt[:15])
+for row in pixels:
+    print(row)
 
